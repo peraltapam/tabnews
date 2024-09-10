54884c3125de45dc8a59ab1510a2c2a9f2911f19
Pamela Peralta <pamperalta@gmail.com>
2024-09-10 01:40:22 +0000

    refactor `.env` and database client logic

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,11 @@
 import migrationsRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     verbose: true,
     dir: join("infra", "migrations"),
@@ -13,6 +15,7 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationsRunner(defaultMigrationOptions);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -21,6 +24,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       return response.status(201).json(migratedMigrations);
