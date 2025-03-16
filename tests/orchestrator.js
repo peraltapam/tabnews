3013d2327eae51e7c788dd6bb6787f8847fca123
Pamela Peralta <pamperalta@gmail.com>
2025-03-16 20:52:38 +0000

    improve async-retry times

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -6,11 +6,14 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 5000,
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
